d8da3315aadf8a92d824e749a8efd67a86ed1496
d7ry <92609548+D7ry@users.noreply.github.com>
2022-01-24 09:33:54 -0800

    merge stopHandler.cpp with hitStop.cpp

--- stopHandler.cpp
+++ stopHandler.cpp
@@ -42,64 +42,8 @@ RE::WEAPON_TYPE getWpnType(RE::Actor* a_actor) {
 } */
 
 
-void stopHandler::calculateStop(bool isPowerAtk, RE::Actor* hitter, RE::TESObjectWEAP* weapon, STOPTYPE stopType) {
-	RE::WEAPON_TYPE wpnType = weapon->GetWeaponType();
-	hitStop::stop(getStopTime(wpnType, stopType, isPowerAtk), getStopSpeed(wpnType, stopType, isPowerAtk), hitter);
-}
 
-float stopHandler::getStopSpeed(RE::WEAPON_TYPE wpnType, STOPTYPE stopType, bool isPower) {
-	float stopSpeed;
-	using namespace settings::stopSpeed;
-	switch (stopType) 
-	{
-	case STOPTYPE::bashStop: stopSpeed = bashStopSpeedPercent; break;
-	case STOPTYPE::blockedStop: stopSpeed = blockedStopSpeedPercent; break;
-	case STOPTYPE::creatureStop: stopSpeed = creatureStopSpeedPercent; break;
-	case STOPTYPE::objectStop: stopSpeed = objectStopSpeedPercent; break;
-	default: stopSpeed = 1;
-	}
-	switch (wpnType)
-	{
-	case RE::WEAPON_TYPE::kHandToHandMelee: stopSpeed *= stopSpeedHandToHandMult; break;
-	case RE::WEAPON_TYPE::kOneHandDagger: stopSpeed *= stopSpeedDaggerMult; break;
-	case RE::WEAPON_TYPE::kOneHandSword: stopSpeed *= stopSpeedSwordMult; break;
-	case RE::WEAPON_TYPE::kOneHandMace: stopSpeed *= stopSpeedMaceMult; break;
-	case RE::WEAPON_TYPE::kOneHandAxe: stopSpeed *= stopSpeedAxeMult; break;
-	case RE::WEAPON_TYPE::kTwoHandAxe: stopSpeed *= stopSpeed2hwMult; break;
-	case RE::WEAPON_TYPE::kTwoHandSword: stopSpeed *= stopSpeedGreatSwordMult; break;
-	}
-	if (isPower) {
-		stopSpeed *= stopSpeedPowerMult;
-	}
-	return stopSpeed;
-}
 
-float stopHandler::getStopTime(RE::WEAPON_TYPE wpnType, STOPTYPE stopType, bool isPower) {
-	float stopTime;
-	using namespace settings::stopTimeMs;
-	switch (stopType)
-	{
-	case STOPTYPE::bashStop: stopTime = bashStopTime; break;
-	case STOPTYPE::blockedStop: stopTime = blockedStopTime; break;
-	case STOPTYPE::creatureStop: stopTime = creatureStopTime; break;
-	case STOPTYPE::objectStop: stopTime = objectStopTime; break;
-	default: stopTime = 1;
-	}
-	switch (wpnType)
-	{
-	case RE::WEAPON_TYPE::kHandToHandMelee: stopTime *= stopTimeHandToHandMult; break;
-	case RE::WEAPON_TYPE::kOneHandDagger: stopTime *= stopTimeDaggerMult; break;
-	case RE::WEAPON_TYPE::kOneHandSword: stopTime *= stopTimeSwordMult; break;
-	case RE::WEAPON_TYPE::kOneHandMace: stopTime *= stopTimeMaceMult; break;
-	case RE::WEAPON_TYPE::kOneHandAxe: stopTime *= stopTimeAxeMult; break;
-	case RE::WEAPON_TYPE::kTwoHandAxe: stopTime *= stopTime2hwMult; break;
-	case RE::WEAPON_TYPE::kTwoHandSword: stopTime *= stopTimeGreatSwordMult; break;
-	}
-	if (isPower) {
-		stopTime *= stopTimePowerMult;
-	}
-	return stopTime;
-}
 
 
 
--- stopHandler.h
+++ stopHandler.h
@@ -2,19 +2,10 @@
 class stopHandler
 {
 public:
-	enum STOPTYPE
-	{
-		objectStop = 0,
-		bashStop = 1,
-		blockedStop = 2,
-		creatureStop = 3
-	};
 
-	static void calculateStop(bool isPowerAtk, RE::Actor* hitter, RE::TESObjectWEAP* weapon, STOPTYPE stopType);
+
 	
-private:
-	static float getStopSpeed(RE::WEAPON_TYPE wpnType, STOPTYPE stopType, bool isPower);
-	static float getStopTime(RE::WEAPON_TYPE wpnType, STOPTYPE stopType, bool isPower);
+	
 };
 
 
